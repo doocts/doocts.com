74d9dca0cf9c14335934f478f405e7797b32d285
moroi <moroiseiya@gmail.com>
2019-06-18 12:55:20 +0900

    Add setting contentful posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,49 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const Promise = require('bluebird')
+const path = require('path')
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, boundActionCreators }) => {
+  const { createPage } = boundActionCreators
+
+  return new Promise((resolve, reject) => {
+    const postTemplate = path.resolve('./src/templates/post.js')
+    resolve(
+      graphql(
+        `
+          {
+            allContentfulPosts {
+              edges {
+                node {
+                  id
+                  title
+                  slug
+                  content {
+                    content
+                  }
+                }
+              }
+            }
+          }
+          `
+      ).then(result => {
+        if (result.errors) {
+          console.log(result.errors)
+          reject(result.errors)
+        }
+
+        const posts = result.data.allContentfulPosts.edges
+        posts.forEach((post, index) => {
+          createPage({
+            path: `/post/${post.node.slug}/`,
+            component: postTemplate,
+            context: {
+              id: post.node.id,
+              title: post.node.title,
+              slug: post.node.slug,
+              content: post.node.content,
+            },
+          })
+        })
+      })
+    )
+  })
+}
---
+++ src/templates/post.js
@@ -0,0 +1,34 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Post = ({ data }) => {
+  const { title, content } = data.contentfulPosts
+  return (
+    <Layout>
+      <SEO title={ title } />
+      <div>
+        <h1>{ title }</h1>
+        <div>{ content.content }</div>
+        <Link to="/">Back to Home</Link>
+      </div>
+    </Layout>
+  )
+}
+
+export default Post
+
+export const pageQuery = graphql`
+  query($slug: String!) {
+    contentfulPosts(slug: { eq: $slug }) {
+      id
+      title
+      slug
+      content {
+        content
+      }
+    }
+  }
+`
